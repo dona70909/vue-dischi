b0ee71a35c77c281fda13d88b9023325fa88dd69
Donatella7 <campaniellodonatella@gmail.com>
2022-03-30 14:33:29 +0200

    start - components - bootstrap

---
+++ src/components/header/HeaderWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1 class="text-danger">Header</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name:"HeaderWrapper"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/main/MainWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        contenuto principale
+    </main>
+</template>
+
+<script>
+export default {
+    name:"MainWrapper",
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
